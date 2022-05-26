a0f2e6a11dc7379b9f541293e24a06e5c250a895
vigneshcivil <jayamvicky1997@gmail.com>
2022-05-26 16:16:07 +0530

    first time

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,18 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+public void deposit() {
+	// TODO Auto-generated method stub
+System.out.println("125%$");
+}
+
+public static void main(String[] args ) {
+	
+AxisBank a = new AxisBank();
+a.fixed();
+a.deposit();
+a.savings();
+
+
+}
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,21 @@
+package org.bank;
+
+public class BankInfo {
+	
+public void savings() {
+
+
+System.out.println("120%");
+}
+public void fixed() {
+
+System.out.println("360%");
+} 
+public void deposit() {
+
+System.out.println("135%");
+}
+
+
+
+}
